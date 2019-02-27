7216481549a495900b4eca5d8456c8d4db3a053f
Prince Daniel A <prindaniel5106@gmail.com>
2019-02-27 11:14:07 +0530

    Update vorc.py

--- vorc.py
+++ vorc.py
@@ -1,6 +1,14 @@
-c = input()
-v = ['a', 'e', 'i', 'o', 'u']
+ch = input()
+v = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+c = [ chr(x) for x in range(65, 91)]
+for x in c:
+  c.append(x.lower())
+for x in c:
+  if x in v:
+    c.remove(x)
 if c in v:
   print("Vowel")
+elif ch in :
+  print("Consonant")
 else:
-  print("Consonent")
+  print("invalid")
